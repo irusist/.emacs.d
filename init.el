777dbee8f7570154d554f495cf81213a96b5efd6
zhulx <zhulx521@gmail.com>
2017-06-09 23:49:24 +0800

    key-keybindings

--- init.el
+++ init.el
@@ -8,8 +8,18 @@
 
 (add-to-list 'load-path "~/.emacs.d/lisp")
 
+(defun open-my-init-file ()
+  (message "aaba")
+  (find-file "~/.emacs.d/init.el"))
+
 (require 'init-packages)
 (require 'init-ui)
+(require 'init-better-defaults)
+(require 'init-org)
+(require 'init-keybindings)
 
-(setq ring-bell-function 'ignore)
+
+(setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
+(load-file custom-file)
+
 
